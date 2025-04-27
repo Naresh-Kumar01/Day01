c4079f74fb6603360c406af64aae1d6fa127f1dd
Nick260624 <nareshkumar260624@gmail.com>
2025-04-28 01:36:26 +0530

    naresh

---
+++ src/array/RemoveDuplicate.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.Scanner;
+
+public class RemoveDuplicate {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("please enter a word");
+		String input =sc.nextLine();
+		sc.close();
+		
+		String result=RemoveDuplicates(input);
+		System.out.println("after removing duplicates:" +result);
+	}
+
+	public static String RemoveDuplicates(String s) {
+		boolean [] seen=new boolean[256];
+		String result="";
+		for(int i=0;i<s.length();i++)
+		{
+			char c=s.charAt(i);
+			if(!seen[c])
+			{
+				result+=c;
+				seen[c]=true;
+			}
+		}
+		return result;
+	}
+	
+}
