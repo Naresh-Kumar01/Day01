84632e4e1107dacf9f182ce6deb185b5626e91d0
Nick260624 <nareshkumar260624@gmail.com>
2025-05-01 18:39:18 +0530

    PalindromNumberProgram

---
+++ src/array/Palindrom_Number_Program.java
@@ -0,0 +1,30 @@
+package array;
+
+public class Palindrom_Number_Program {
+public static void main(String[] args) {
+		
+		int r,sum=0, temp;
+		
+		int num=464;
+		
+		temp=num;
+		
+		while (num>0)
+		{
+			r=num%10;
+			sum=(sum*10)+r;
+			num=num/10;
+		}
+		if(temp==sum)
+		{
+			System.out.println("Number is Palindrome ");
+		}
+		else
+		{
+			System.out.println("Numbers is not plaindrome ");
+		}
+
+	}
+
+
+}
